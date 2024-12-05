04983a5e959cae609477ab853c2d18781dc1d485
Azzni96 <nihadazzam96@gmail.com>
2024-12-05 17:55:13 +0200

    env install

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,10 +1,16 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config(); // Lataa .env-tiedoston muuttujat
 
-// Set up the MariaDB connection
-const sequelize = new Sequelize('HEALTHYSUPP', 'root', 'Nihad1996', {
-  host: 'localhost',
-  dialect: 'mariadb',
-  logging: false,
-});
+// Set up the MariaDB connection using environment variables
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    logging: false,
+  }
+);
 
 module.exports = sequelize;
